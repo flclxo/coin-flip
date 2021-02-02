37c319239e2de7db6bef763c629ce472569acce6
flclxo <36686123+flclxo@users.noreply.github.com>
2021-02-01 22:08:18 -0500

    Coin Flip

---
+++ Main.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+       
+        Scanner scan = new Scanner(System.in);
+        Random game = new Random();
+
+        System.out.println("Heads or Tails?");
+
+        String headsOrTails = scan.nextLine();
+
+        int toss = game.nextInt(2) + 1;
+        
+        if (toss == 1) {
+            System.out.println("Heads  is the outcome of the coin toss. Thank you for playing!");
+        }
+
+        else {
+            System.out.println("Tails is the outcome of the coin toss. Thankyou for playing!");
+        }
+
+    }
+}
